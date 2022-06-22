03a70431d190c3c27d6142ef9d820df70d64eb04
mahmoud khalifa <macintosh@Macintoshs-MacBook-Pro.local>
2022-06-22 14:12:53 -0700

    add design to Latest Cell

--- sports app 2/sports app/modules/Details Of League/views/cells/Latest cell/LatestResultCollectionViewCell.swift
+++ sports app 2/sports app/modules/Details Of League/views/cells/Latest cell/LatestResultCollectionViewCell.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class LatestResultCollectionViewCell: UICollectionViewCell {
-
+    @IBOutlet weak var firstTeam: UILabel!
+    @IBOutlet weak var firstTeamScore: UILabel!
+    
+    @IBOutlet weak var dateOfMatch: UILabel!
+    @IBOutlet weak var secondTeam: UILabel!
+    @IBOutlet weak var secondTeamScore: UILabel!
+    @IBOutlet weak var timeOfmatch: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
